48daa159ddeb89eec6c59c5bb624dca554fdafee
David Ryan Hall <david.ryan.hall@gmail.com>
2025-02-05 18:15:06 -0800

    ui: style the logout page, so the user is aware why they are being re-routed

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -7,9 +7,15 @@ export default function Logout(): ReactNode {
   const {logout} = useAuthContext();
   setTimeout(() => {
     logout();
-  }, 1000);
+  }, 2000);
 
   return (
-    <p>Logout</p>
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <div className="w-60 border border-orange-400 rounded-lg p-4">
+        <h1 className="font-bold text-center text-xl text-indigo-950 pb-2">Session Expired</h1>
+        <p className="text-indigo-950">Your session  has expired. You will be routed to the login page to re-authenticate</p>
+      </div>
+    </div>
+
   );
 }
