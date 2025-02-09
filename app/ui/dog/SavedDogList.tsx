04361afb7e424a86385e572201769bd5e3d7a81e
David Ryan Hall <david.ryan.hall@gmail.com>
2025-02-08 23:06:58 -0800

    feat(bookmarks): add a button to clear all saved dogs

--- app/ui/dog/SavedDogList.tsx
+++ app/ui/dog/SavedDogList.tsx
@@ -4,19 +4,35 @@ import { useBookmarkContext } from "@context/bookmarkProvider";
 import { ReactNode } from "react";
 import NoSavedDogs from "@ui/dog/NoSavedDogs";
 import DogCard from "@ui/dog-search/DogCard";
+import Button from "@ui/common/Button";
 
 export default function SavedDogs(): ReactNode {
-  const { bookmarks } = useBookmarkContext();
+  const { bookmarks, clearBookmarks } = useBookmarkContext();
 
   if (!bookmarks || Object.keys(bookmarks).length === 0) {
     return <NoSavedDogs />
   }
 
   return (
-    <>
-      {Object.keys(bookmarks).map((dogId) => (
-        <DogCard key={dogId} dog={bookmarks[dogId]} />
-      ))}
-    </>
+    <div className="flex flex-col justify-center">
+      <div className="flex flex-wrap overflow-scroll no-scrollbar">
+        {Object.keys(bookmarks).map((dogId) => (
+          <DogCard key={dogId} dog={bookmarks[dogId]} />
+        ))}
+      </div>
+
+      <div className="mt-5 flex w-full justify-center">
+        <Button
+          type="button"
+          theme="primary"
+          onClick={clearBookmarks}
+        >
+          <p className="text-xs">
+            Clear all saved pups
+          </p>
+        </Button>
+      </div>
+
+    </div>
   );
 }
