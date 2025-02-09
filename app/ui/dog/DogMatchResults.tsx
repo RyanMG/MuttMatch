049cef216ed748c1cef551177fe91002265ee2d6
David Ryan Hall <david.ryan.hall@gmail.com>
2025-02-09 08:55:59 -0800

    feat(match): fill out the match page
     - Unauthorized error on fetching match now also routes user to /logout

--- app/ui/dog/DogMatchResults.tsx
+++ app/ui/dog/DogMatchResults.tsx
@@ -5,17 +5,21 @@ import { getDogMatchById } from "@api/dogs";
 import PageLoading from "@ui/common/PageLoading";
 import { useBookmarkContext } from "@context/bookmarkProvider";
 import { TDog } from "@definitions/dogs";
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 export default function DogMatchResults(): ReactNode {
   const { bookmarks } = useBookmarkContext();
   const [match, setMatch] = useState<TDog | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
     (async() => {
       if (bookmarks && Object.keys(bookmarks).length > 0) {
         const response = await getDogMatchById(Object.keys(bookmarks));
         if (response === "Unauthorized") {
-          return "Unauthorized";
+          router.push('/logout');
+          return;
         }
 
         if ('error' in response) {
@@ -32,8 +36,18 @@ export default function DogMatchResults(): ReactNode {
   }
 
   return (
-    <div>
-      <p className="text-lg text-indigo-950">{match.name}</p>
+    <div className="flex flex-col items-center mt-5">
+      <h1 className="text-2xl font-bold text-indigo-950">We found your perfect Mutt Match!</h1>
+      <section className="flex flex-row gap-2 my-4">
+        <Image
+          src={match.img}
+          alt={match.name}
+          width={300}
+          height={300}
+        />
+      </section>
+      <p className="text-2xl font-bold text-orange-500">{`Meet ${match.name}!`}</p>
+      <p className="text-md italic text-center text-indigo-950">{`${match.name!} the lovable ${match.breed!} is ready to meet you in the ${match.zip_code!} area.`}</p>
     </div>
   )
 }
